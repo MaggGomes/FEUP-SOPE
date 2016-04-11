631556edb6a3c22b2fb9a1f71d965ec1df71a472
JJPO96 <jose.j.p.oliveira.96@gmail.com>
2016-04-11 18:07:00 +0100

    lsdir not finished version

--- Project1/lsdir.c
+++ Project1/lsdir.c
@@ -1,32 +1,63 @@
-#include <stdlib.h>
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
 
+#define BUF_LENGTH 512
 
-int main(int argc, char *argv[])
-{
-	// TODO - criar função separada para diretorio e abrir
-	/*
+int main(int argc, char *argv[]){
+	pid_t pid;
+	DIR * inDir = opendir(argv[1]);
+	struct dirent *tempDir	= readdir(inDir);
+	struct stat stat_buf;
+	char diretorio[BUF_LENGTH];
 
-	 DIR* dir;
-	 dirp = opendir(argv[1]);
+	if(argc != 2){
+		printf("Invalid Arguments");
+		return 1;
+	}
 
-    if (dirp == NULL)
-    {
-        fprinft(stderr, "Error:: Invalid directory path. Did you forget the '/' at end?", strerror(erro));
-        return 2;
+	if(inDir == NULL){
+		printf("Invalid Directory");
+		return 1;
+	}
+
+	while((tempDir = readdir(inDir)) != NULL){
+		sprintf(diretorio, "%s/%s", argv[1], tempDir->d_name);
+
+		if (lstat(diretorio, &stat_buf) < 0){
+			perror(tempDir->d_name);
+      closedir(inDir);
+      return 1;
     }
 
-	 */
+		if(S_ISREG(stat_buf.st_mode)){
 
+			printf("%s\n",diretorio);
 
-	// TODO apenas para verificar se está a entrar em lsdir
-	fprintf(stdout, "entrou em lsdir");
+		}else if ((S_ISDIR(stat_buf.st_mode)) && strcmp(tempDir->d_name,".") && strcmp(tempDir->d_name,"..")){
 
+			pid = fork();
 
+			if(pid < 0){
+				printf("Fork failed");
+				return 1;
+			}else if (pid == 0) {
 
+				printf("%s\n",diretorio);
+				execl("lsdir", "lsdir", (diretorio), NULL);
+				closedir(inDir);
+				return 0;
+			}
+		}
+	}
 
-	//perror(strerror(errno));
-	//perror("Error getting data about directory.");
-
+	closedir(inDir);
 	return 0;
-} 
+}
