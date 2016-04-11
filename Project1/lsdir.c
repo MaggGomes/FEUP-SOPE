3e4a21635253a6e0ad47ff52192d432787e35ce0
MaggGomes <magg.slb@gmail.com>
2016-04-11 14:54:50 +0100

    first commit

---
+++ Project1/lsdir.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(int argc, char *argv[])
+{
+	// TODO - criar função separada para diretorio e abrir
+	/*
+
+	 DIR* dir;
+	 dirp = opendir(argv[1]);
+
+    if (dirp == NULL)
+    {
+        fprinft(stderr, "Error:: Invalid directory path. Did you forget the '/' at end?", strerror(erro));
+        return 2;
+    }
+
+	 */
+
+
+	// TODO apenas para verificar se está a entrar em lsdir
+	fprintf(stdout, "entrou em lsdir");
+
+
+
+
+	//perror(strerror(errno));
+	//perror("Error getting data about directory.");
+
+	return 0;
+} 
---
+++ Project1/rmdup.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+const char* fileName = "files.txt";
+const char* hlinksName = "hlinks.txt";
+
+int main(int argc, char *argv[])
+{
+
+	// TODO usar return ou exit()
+	if (argc < 2 ){
+		fprintf(stderr, "Error:: Invalid number of arguments. Did you forget to mention the <directory path> ? Please invoke the program as follows: rmdup <directory path>\n\n");
+		return 1;
+	}
+
+	else if (argc > 2 ){
+		fprintf(stderr, "Error:: Invalid number of arguments. Please invoke the program as follows: rmdup <directory path>\n\n");
+		return 2;
+	}
+
+	// TODO Usar função open ou dup2(fd, STDOUT_FILENO) para redirecionamento de stream
+	FILE* file;
+	file = fopen(fileName, "w+");
+
+	pid_t pid = fork();
+
+	if (pid == -1) {
+		fprintf(stderr, "\nError: Fork failed!\n");
+		return 3;
+	}
+
+	else if (pid == 0) {
+		// Starts a new child process, executing lsdir program
+		execlp("./lsdir", "lsdir", argv[1], NULL);
+
+		// Line below only is executed if lsdir fails to execute
+		fprintf(stderr, "Error:: Can't execute lsdir: %s.\n", strerror(errno));
+	}
+	else {
+		int status;
+		(void)waitpid(pid, &status, 0);
+	}
+
+	fclose(file);
+
+	return 0;
+} 
