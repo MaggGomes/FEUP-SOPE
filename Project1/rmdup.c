a28d631c05304e37253924d47a62a5b6b8630c79
MaggGomes <magg.slb@gmail.com>
2016-04-11 20:12:13 +0100

    minor changes

--- Project1/rmdup.c
+++ Project1/rmdup.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/wait.h>
 
 const char* fileName = "files.txt";
 const char* hlinksName = "hlinks.txt";
@@ -20,31 +21,26 @@ int main(int argc, char *argv[])
 		return 2;
 	}
 
-	// TODO Usar função open ou dup2(fd, STDOUT_FILENO) para redirecionamento de stream
-	FILE* file;
-	file = fopen(fileName, "w+");
+	FILE * file = fopen(fileName, "a+");
 
 	pid_t pid = fork();
 
 	if (pid == -1) {
-		fprintf(stderr, "\nError: Fork failed!\n");
+		fprintf(stderr, "Error: Fork failed!\n");
 		return 3;
 	}
 
-	else if (pid == 0) {
-		// Starts a new child process, executing lsdir program
-		remove("files.txt");
+	else if (pid == 0) { // child
+		// Starts a new process, executing lsdir program on it
 		execlp("./lsdir", "lsdir", argv[1], NULL);
 
 		// Line below only is executed if lsdir fails to execute
 		fprintf(stderr, "Error:: Can't execute lsdir: %s.\n", strerror(errno));
 	}
-	else {
-		int status;
-		(void)waitpid(pid, &status, 0);
+	else { // parent
+		waitpid(pid, NULL, 0);
+		fclose(file);
 	}
-
-	fclose(file);
 
 	return 0;
 }
