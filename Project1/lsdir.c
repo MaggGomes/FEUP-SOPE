d141222ebfd3cfec06b6d0c346da06f6e770d899
MaggGomes <magg.slb@gmail.com>
2016-04-13 04:47:29 +0100

    sort now working

--- Project1/lsdir.c
+++ Project1/lsdir.c
@@ -11,37 +11,55 @@
 
 #define BUF_LENGTH 512
 
-int explore_directory(const char* dirPath, const char* filePath){
+int explore_directory(int file, const char* dirPath, const char* filePath);
+
+int main(int argc, char *argv[]){
+
+	int file;
+
+	if (argc != 3)
+		fprintf(stderr, "Wrong number of arguments. Usage: %s <directory path> <save file path>.\n", argv[0]);
+
+	if ((file = open(argv[2], O_WRONLY | O_APPEND | O_CREAT | O_TRUNC, S_IRWXU)) == -1){
+		fprintf(stderr, "Failed to create %s.", argv[2]);
+		exit(1);
+	}
+
+	explore_directory(file, argv[1], argv[2]);
+
+	close(file);
+
+	return 0;
+}
+
+int explore_directory(int file, const char* dirPath, const char* filePath){
 
 	struct dirent *dirStream;
 	struct stat fileStat;
 	pid_t pid;
 	DIR * inDir;
-	char path[BUF_LENGTH];
+	char path[BUF_LENGTH], buffer[BUF_LENGTH];
 	int status;
-	FILE * output;
 
 	if((inDir = opendir(dirPath)) == NULL){
-		fprintf(stderr, "Error:: Couldn't open the directory <%s>.\n", dirPath);
-		exit(1);
+		fprintf(stderr, "Couldn't open the directory <%s>.\n", dirPath);
+		exit(2);
 	}
 
 	while((dirStream = readdir(inDir)) != NULL){
 		sprintf(path, "%s/%s", dirPath, dirStream->d_name);
 
 		if (lstat(path, &fileStat) < 0){
-			fprintf(stderr, "Error:: %s.\n", strerror(errno));
+			fprintf(stderr, "%s.\n", strerror(errno));
 			closedir(inDir);
-			exit(2);
+			exit(3);
 		}
 
+		// Verifies if is a regular file
 		if(S_ISREG(fileStat.st_mode)){
-			output = fopen(filePath,"a");
-			//name_of_the_file    complete_path    date_of_last_mod   size_in_bytes    mode_of_the_file
-			fprintf(output,"%-20s %40s %d %d %d\n", dirStream->d_name, path, (int) fileStat.st_mtime, (int) fileStat.st_size, (int) fileStat.st_mode);
-			fclose(output);
+			sprintf(buffer,"%-20s %40s %d %d %d\n", dirStream->d_name, path, (int) fileStat.st_mtime, (int) fileStat.st_size, (int) fileStat.st_mode);
+			write(file, buffer, strlen(buffer));
 		}
-
 
 		// Avoids open current and parent directorys
 		else if ((S_ISDIR(fileStat.st_mode)) && strcmp(dirStream->d_name,".")!=0
@@ -50,33 +68,21 @@ int explore_directory(const char* dirPath, const char* filePath){
 			pid = fork();
 
 			if(pid == -1){
-				fprintf(stderr, "Error: Fork failed!\n");
-				exit(3);
-			}else if (pid == 0) { // child
-				explore_directory(path, filePath);
+				fprintf(stderr, "Fork failed!\n");
 				exit(4);
+			}else if (pid == 0) { // child
+				explore_directory(file, path, filePath);
+				exit(5);
 			}
 
 			else{ // parent
 				if (waitpid(pid, &status, 0) == -1)
-					fprintf(stderr, "Error:: %s.\n", strerror(errno));
+					fprintf(stderr, "%s.\n", strerror(errno));
 			}
 		}
 	}
 
 	closedir(inDir);
-
-	return 0;
-}
-
-int main(int argc, char *argv[]){
-
-	if (argc != 3)
-		fprintf(stderr, "ERROR:: Wrong number of arguments. Usage: %s <directory path> <save file path>.\n", argv[0]);
-
-	// Clears the file where the files will be saved
-	open(argv[2], O_TRUNC);
-	explore_directory(argv[1], argv[2]);
 
 	return 0;
 }
--- Project1/rmdup.c
+++ Project1/rmdup.c
@@ -1,44 +1,122 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-#include <errno.h>
 #include <string.h>
+#include <dirent.h>
+#include <sys/stat.h>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <fcntl.h>
 #include <sys/wait.h>
+
+#define READ 0
+#define WRITE 1
+#define BUF_LENGTH 512
 
 const char* filePath = "./files.txt";
 const char* hlinksPath = "./hlinks.txt";
 
-int main(int argc, char *argv[])
-{
+int sort_file(const char* fileName);
+
+int main(int argc, char* argv[]) {
+
 	pid_t pid = fork();
 	int status;
 
-	// TODO avaliar possibilidade de erro -1 na abertura do open()
 	if (argc < 2 ){
-		fprintf(stderr, "Error:: Invalid number of arguments. Did you forget to mention the <directory path> ? Usage: %s <directory path>\n", argv[0]);
+		fprintf(stderr, "Invalid number of arguments. Did you forget to mention the <directory path> ? Usage: %s <directory path>\n", argv[0]);
 		exit(1);
 	}
 
 	else if (argc > 2 ){
-		fprintf(stderr, "Error:: Invalid number of arguments. Usage: %s <directory path>\n", argv[0]);
+		fprintf(stderr, "Invalid number of arguments. Usage: %s <directory path>\n", argv[0]);
 		exit(2);
 	}
 
 	if (pid == -1) {
-		fprintf(stderr, "Error: Fork failed!\n");
+		fprintf(stderr, "Fork failed!\n");
 		exit(3);
 	}
 
 	else if (pid == 0) { // child
 		// Starts a new process, executing lsdir program on it
-		execlp("./lsdir", "lsdir", argv[1], filePath, NULL);
+		if (execlp("./lsdir", "lsdir", argv[1], filePath, NULL) == -1){
+			fprintf(stderr, "execlp error: %s", strerror(errno));
+			exit(4);
+		}
 
 		// Line below only is executed if lsdir fails to execute
-		fprintf(stderr, "Error:: Can't execute lsdir: %s.\n", strerror(errno));
+		fprintf(stderr, "Can't execute lsdir: %s.\n", strerror(errno));
 	}
 	else { // parent
-		if (waitpid(pid, &status, 0) == -1)
-			fprintf(stderr, "Error:: %s.\n", strerror(errno));
+		if (waitpid(pid, &status, 0) == -1){
+			fprintf(stderr, "%s.\n", strerror(errno));
+			exit(5);
+		}
+
+		sort_file(filePath);
+	}
+
+	return 0;
+}
+
+int sort_file(const char* fileName)
+{
+	int n, fd[2];
+	int file, status;
+	char buffer[BUF_LENGTH];
+	pid_t pid;
+
+	if (pipe(fd) != 0) {
+		fprintf(stderr, "Pipe error,\n");
+		exit(7);
+	}
+
+	if ((pid = fork()) == -1) {
+		fprintf(stderr, "Fork failed!\n");
+		exit(8);
+	}
+
+	else if (pid == 0) { // child
+		close(fd[READ]); // Closes reading side
+
+		if ((file = open(fileName, O_RDONLY, S_IRWXU)) == -1){
+			fprintf(stderr, "Failed to open %s.", fileName);
+			exit(9);
+		}
+
+		while ((n = read(file, buffer, BUF_LENGTH)) != 0) {
+			if (write(fd[WRITE], buffer, n) != n) {
+				fprintf(stderr, "write error to pipe\n");
+				exit(10);
+			}
+		}
+
+		close(fd[WRITE]); // Closes writing side
+		close(file);
+	}
+	else { // parent
+		if (waitpid(pid, &status, 0) == -1){
+			fprintf(stderr, "%s.\n", strerror(errno));
+			exit(11);
+		}
+
+		close(fd[WRITE]); // Closes writing side
+		dup2(fd[READ], STDIN_FILENO);
+		close(fd[READ]); // Closes reading side
+
+		if ((file = open(fileName, O_RDWR | O_CREAT , S_IRWXU)) == -1){
+			fprintf(stderr, "Failed to create %s.", fileName);
+			exit(12);
+		}
+
+		dup2(file, STDOUT_FILENO);
+		if (execlp("sort", "sort", NULL) == -1){
+			fprintf(stderr, "execlp error: %s", strerror(errno));
+			exit(13);
+		}
+
+		close(file);
 	}
 
 	return 0;
