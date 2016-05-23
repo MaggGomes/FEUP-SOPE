031c6a5607f59a65bd04285818bf095aba02a51a
MaggGomes <magg.slb@gmail.com>
2016-05-23 22:10:02 +0100

    several changes

--- Project2/parque.c
+++ Project2/parque.c
@@ -10,21 +10,14 @@
 #include <pthread.h>
 #include "utilities.h"
 
-#define NUM_CONTROLLERS 4
-
 // Global variables
-const char* LOG_FILENAME = "parque.log";
-const char* LOG_FULL = "cheio";
-const char* LOG_CLOSED = "encerrado";
-const char* LOG_PARKING = "estacionamento";
-const char* LOG_EXIT= "saida";
-char* fifoControllers[NUM_CONTROLLERS] = {"./temp/fifoN", "./temp/fifoS", "./temp/fifoE", "./temp/fifoO"};
-
 static int numPlaces;
 static int openTime;
 static int occupiedPlaces = 0;
 static clock_t startT;
 static pthread_mutex_t mutexPark = PTHREAD_MUTEX_INITIALIZER;
+static FILE* fdLog;
+const char* LOG_FILENAME = "parque.log";
 
 /**
 * @brief Receives the vehicles
@@ -45,14 +38,22 @@ void* car_park(void* arg);
 *
 * @param pathName where the fifo will be created
 * @param flag used to open the fifo
-* @return the file descritor of the fifo if no error has ocurred; -1 otherwise
+* @return the file descriptor of the fifo if no error has ocurred; -1 otherwise
 */
 int create_fifo(char* pathName, int flag);
 
 /**
-* Informs all the controllers to stop receiving cars
+* @brief Informs all the controllers to stop receiving cars
 */
 void close_park();
+
+/**
+* @brief Write to the log the vehicle info
+*
+* @param vehicle
+* @param msg to be written
+*/
+void write_to_log(vehicle_t vehicle, const char* msg);
 
 int main (int argc, char * argv[]){
 
@@ -68,7 +69,7 @@ int main (int argc, char * argv[]){
 	numPlaces = strtol(argv[1], NULL, 10);
 	if (errno == ERANGE || errno == EINVAL) {
 		perror("convert N_LUGARES failed");
-		exit(3);
+		exit(2);
 	}
 
 	errno = 0;
@@ -78,85 +79,145 @@ int main (int argc, char * argv[]){
 		exit(3);
 	}
 
-	// TODO - ABRIR O FICHEIRO LOGGER
+	if ( (fdLog = fopen(LOG_FILENAME, "w")) == NULL ){
+		perror(LOG_FILENAME);
+		exit(4);
+	}
 
-	startT = clock();
+	fprintf(fdLog, "t(ticks) ; nlug ; id_viat ; observ\n");
 
+	startT = clock(); // Current time
+
+	// Creates a thread for each controller
 	for (i = 0; i  < NUM_CONTROLLERS; i++){
 		pthread_create(&controllerThreads[i], NULL, controller, fifoControllers[i]);
 	}
 
-	sleep(10); // Park is open
+	sleep(openTime); // Park is open
 
-	// Closes park
-	close_park();
+	close_park(); // Closes park
 
 	// Waits for the thread of each park's controller
 	for (i = 0; i < NUM_CONTROLLERS; i++){
 		pthread_join(controllerThreads[i], NULL);
 	}
 
-	// TODO - CRIAR ESTATISTICAS
+	fclose(fdLog);
 
 	pthread_exit(0);
 }
 
-// TODO - FAZER
 void* controller(void* arg){
 
 	int fd, n;
 	char* fifoName;
-	pthread_t carSaverID;
+	vehicle_t vehicle;
+
+	pthread_t carParkID;
 
 	fifoName = (char *) arg;
 
+	// Creates FIFO
 	if ( (fd = create_fifo(fifoName, O_RDONLY)) == -1 ){
 		perror(fifoName);
 		pthread_exit(NULL);
 	}
 
-	// TODO - TEMP - APAGAR
-	char string[200];
-
-	while (1) {
-
-		n = read(fd, string, 200);
-
+	// Reads FIFO
+	while ((n = read(fd, &vehicle, sizeof(vehicle_t))) >= 0) {
+		// If a vehicle is received
 		if (n > 0)
 		{
-			// TODO - CORRIGIR CODIGO NESTA PARTE
-			printf("%s\n", string);
-
-			if (strcmp(string, "finish") == 0)
-			break;
-
-			pthread_create(&carSaverID, NULL, car_park, string);
-		}
-		else if (n == -1)
-		{
-			perror(fifoName);
-			close(fd);
-			unlink(fifoName);
-			return NULL;
+			if (vehicle.stopVehicle == 1){
+				break;
+			}
+			// Creates a new thread for each car to park it if possible
+			else
+			pthread_create(&carParkID, NULL, car_park, &vehicle);
 		}
 	}
 
+	// If an error has ocurred reading the FIFO
+	if (n < 0)
+	perror(fifoName);
+
 	close(fd);
-	unlink(fifoName);
+
+	if (unlink(fifoName) == -1)
+	perror(fifoName);
 
 	pthread_exit(NULL);
 }
 
 // TODO - FAZER
 void* car_park(void* arg){
+
+	int fd;
+	char msg[BUF_LENGTH];
+	vehicle_t vehicle = *(vehicle_t *) arg;
 	pthread_t selfThread = pthread_self();
 
+	// Makes the thread detached
 	if (pthread_detach(selfThread) != 0){
 		perror("Failed to make a thread detached.\n");
-		exit(1);
+		return NULL;
 	}
 
-	pthread_exit(NULL);
+	if ((fd = open(vehicle.inf.fifoName, O_WRONLY  | O_NONBLOCK)) == -1){
+		perror(vehicle.inf.fifoName);
+		return NULL;
+	}
+
+	/*
+
+
+	// Validar entrada (Zona critica)
+	pthread_mutex_lock(&arrumador_lock);
+
+	if (numLugaresOcupados < numLugares)
+	{
+	accepted = 1;
+	numLugaresOcupados++;
+	strcpy(feedback.msg, ACCEPTED_STR);
+	park_log(info, LOG_ACCEPTED_STR);
+}
+else {
+strcpy(feedback.msg, FULL_STR);
+park_log(info, LOG_FULL_STR);
+}
+
+pthread_mutex_unlock(&arrumador_lock);
+/*********************************/
+
+/*  write(fd_vehicle, &feedback, sizeof(feedback));
+
+if (accepted)
+{
+feedback_t exit_feedback;
+
+// Esperar
+wait_ticks(info.parking_time);
+
+// Validar saida (Zona critica)
+pthread_mutex_lock(&arrumador_lock);
+
+numLugaresOcupados--;
+strcpy(exit_feedback.msg, EXITING_STR);
+park_log(info, LOG_EXITING_STR);
+
+pthread_mutex_unlock(&arrumador_lock);
+/*********************************/
+
+/*  write(fd_vehicle, &exit_feedback, sizeof(exit_feedback));
+}
+
+
+free(arg);
+close(fd_vehicle);
+return NULL;*/
+
+
+return NULL;
 }
 
 int create_fifo(char* pathName, int flag) {
@@ -182,16 +243,28 @@ int create_fifo(char* pathName, int flag) {
 	return fd;
 }
 
-// TODO - CORRIGIR
 void close_park(){
-	int i;
+	int fd, i;
+	vehicle_t vehicle;
+	vehicle.stopVehicle = 1; // Stop vehicle
 
 	for (i = 0; i < NUM_CONTROLLERS; i++){
-		int fd = open(fifoControllers[i], O_WRONLY  | O_NONBLOCK);
-		if (fd == -1)
-		printf("%s", "error");
+		if ((fd = open(fifoControllers[i], O_WRONLY  | O_NONBLOCK)) == -1){
+			perror(fifoControllers[i]);
+			return;
+		}
 
-		write(fd, "finish", sizeof("finish"));
+		write(fd, &vehicle, sizeof(vehicle_t));
 		close(fd);
 	}
+}
+
+void write_to_log(vehicle_t vehicle, const char* msg){
+	char logMsg[BUF_LENGTH];
+	// TODO - CORRIGIR
+	sprintf(logMsg, "%8ld ; %4d ; %7d ; %s\n",
+	clock() - startT, 0, vehicle.inf.v_id, "cheio");
+
+	// Writes to the log
+	fprintf(fdLog, logMsg, strlen(logMsg));
 }
--- Project2/utilities.h
+++ Project2/utilities.h
@@ -1,7 +1,18 @@
 #ifndef UTILITIES_H
 #define UTILITIES_H
 
+#define NUM_CONTROLLERS 4
 #define BUF_LENGTH 50
+
+// Status messages
+const char* ENTRY_PARK = "entrada";
+const char* EXIT_PARK = "saida";
+const char* FULL = "cheio!";
+const char* LOG_FULL = "cheio";
+const char* CLOSED = "encerrado";
+
+// Controller's FIFO
+char* fifoControllers[NUM_CONTROLLERS] = {"/tmp/fifoN", "/tmp/fifoS", "/tmp/fifoE", "/tmp/fifoO"};
 
 typedef struct {
   clock_t parked_time;
@@ -11,6 +22,7 @@ typedef struct {
 
 typedef struct {
   char fifoName[BUF_LENGTH];
+  int stopVehicle; // 1 if is a stop vehicle,; 0 otherwise
   info_t inf;
 } vehicle_t;
 
